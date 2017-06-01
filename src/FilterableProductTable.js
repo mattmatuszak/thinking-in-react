3106fb9b253ea2f183c2ee9d2236402944286efb
MattVobejda <mattvobejda@northwesternmutual.com>
2017-06-01 13:02:17 -0500

    Awesome updates form HMM

--- src/FilterableProductTable.js
+++ src/FilterableProductTable.js
@@ -6,23 +6,34 @@ import ProductTable from './ProductTable';
 
 
 class FilterableProductTable extends Component {
+    state = {
+        onlyInStockItems: false,
+        search: false
+    }
 
+    updateInStockItems = (event) => {
+        
+        this.setState(Object.assign(this.state,{onlyInStockItems: true}))
+    }
 
+    render() {
+        let products = this.props.products;
 
+        if(this.state.onlyInStockItems === true){
+            products = this.props.products.filter((val) => {
+                return val.stocked
+            });
+        }
+        console.log(products);
 
-  render() {
-
-      console.log(this.props);
-
-
-    return (
-      <div className="App">
-        <h1>hello </h1>
-        <SearchBar></SearchBar>
-        <ProductTable products={this.props.products}></ProductTable>
-      </div>
-    );
-  }
+        return (
+          <div className="App">
+            <h1>hello </h1>
+            <SearchBar onlyInStockItems={this.state.onlyInStockItems} updateInStockItems={this.updateInStockItems}></SearchBar>
+            <ProductTable products={products}></ProductTable>
+          </div>
+        );
+    }
 }
 
 export default FilterableProductTable;
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -3,11 +3,14 @@ import React, { Component } from 'react';
 
 
 class SearchBar extends Component {
+  
+
+
   render() {
     return (
       <div>
         <input /><br />
-        <input type="checkbox" /><label>Only show products in stock</label>
+        <input type="checkbox" onChange={this.props.updateInStockItems}/><label>Only show products in stock</label>
       </div>
     );
   }
